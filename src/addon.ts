04487982e318f813448add3233db102ded319b48
ChenglongMa <github-action@actions-template-sync.noreply.github.com>
2024-03-01 01:01:58 +0000

    chore(template): merge template changes :up:

--- src/addon.ts
+++ src/addon.ts
@@ -2,12 +2,11 @@ import { ColumnOptions } from "zotero-plugin-toolkit/dist/helpers/virtualizedTab
 import { DialogHelper } from "zotero-plugin-toolkit/dist/helpers/dialog";
 import hooks from "./hooks";
 import { createZToolkit } from "./utils/ztoolkit";
-import { Action } from "./utils/prefs";
 
 class Addon {
   public data: {
     alive: boolean;
-    // Env type, see build.mjs
+    // Env type, see build.js
     env: "development" | "production";
     ztoolkit: ZToolkit;
     locale?: {
@@ -15,13 +14,10 @@ class Addon {
     };
     prefs?: {
       window: Window;
-      columns?: Array<ColumnOptions>;
-      rows?: Array<{ [dataKey: string]: string }>;
+      columns: Array<ColumnOptions>;
+      rows: Array<{ [dataKey: string]: string }>;
     };
-    dialogs: {
-      dialog?: DialogHelper;
-      duplicateMaps?: Map<number, { existingItemIDs: number[]; action: Action }>;
-    };
+    dialog?: DialogHelper;
   };
   // Lifecycle hooks
   public hooks: typeof hooks;
@@ -33,7 +29,6 @@ class Addon {
       alive: true,
       env: __env__,
       ztoolkit: createZToolkit(),
-      dialogs: {},
     };
     this.hooks = hooks;
     this.api = {};
